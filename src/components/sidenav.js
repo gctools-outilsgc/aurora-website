06ab2537e72de262f11c2ebb7db65dd73810d76a
jerjerzanne <simon.roy5219@gmail.com>
2018-08-08 12:09:07 -0700

    transformed sidebar into a class component

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import LocalizedComponent
@@ -10,149 +10,151 @@ import {
 } from 'reactstrap';
 
 
-const Sidenav = (props) => (
-  <StaticQuery query={graphql`
-  query{
-    component:allMarkdownRemark(
-      filter: {
-        frontmatter: {path: {regex: "/component/"}}
-      },
-      sort:{
-        fields:[frontmatter___num]
-      }
-    ){
-      totalCount
-        edges{
-          node {
-            frontmatter {
-              title
-              path
-              lang
-              num
-            }
-          }
-      }
-    }
-    overview:allMarkdownRemark(
-      filter: 
-        {frontmatter: {path: {regex: "/overview/"}}
-      },
-      sort:{
-        fields:[frontmatter___num]
-      }
-    ){
-      totalCount
-        edges{
-          node {
-            frontmatter {
-              title
-              path
-              lang
-              num
-            }
-          }
-      }
-    }
-    content:allMarkdownRemark(
-      filter: 
-      {frontmatter: {path: {regex: "/content/"}}
-      },
-      sort:{
-        fields:[frontmatter___num]
-      }
-    ) {
-      totalCount
-        edges{
-          node {
-            frontmatter {
-              title
-              path
-              lang
-              num
-            }
-          }
-      }
-    }
-    identity:allMarkdownRemark(
-      filter: {
-        frontmatter: {path: {regex: "/identity/"}}
-      },
-      sort:{
-        fields:[frontmatter___num]
-      }
-    ) {
-      totalCount
-        edges{
-          node {
-            frontmatter {
-              title
-              path
-              lang
-              num
-            }
-          }
-      }
-    }
-    data:allMarkdownRemark(
-      filter: {
-        frontmatter: {path: {regex: "/data/"}}
-      },
-      sort:{
-        fields:[frontmatter___num]
-      }
-    ){
-      totalCount
-        edges{
-          node {
-            frontmatter {
-              title
-              path
-              lang
-              num
-            }
-          }
-      }
-    }
+class Sidenav extends Component {
+  constructor(props) {
+    super(props);
   }
-    `}
-    render={data => (
-      <div className="col-sm-auto">
-        <Nav vertical pills justify>
-          {
-            data[props.path.split("/")[1]].edges.map((edges) => {
-              if ((localizer.lang === "en_CA") && (edges.node.frontmatter.lang === "en"))
-                return (
-                  <NavItem>
-                    <Link to={edges.node.frontmatter.path} class="nav-link">
-                      {edges.node.frontmatter.title}
-                    </Link>
-                  </NavItem>
-                );
-              else
-                if ((localizer.lang === "fr_CA") && (edges.node.frontmatter.lang === "fr"))
-                  return (
-                    <NavItem>
-                      <Link to={edges.node.frontmatter.path} class="nav-link">
-                        {edges.node.frontmatter.title}
-                      </Link>
+  render() {
+    return (
 
-                    </NavItem>);
+      <StaticQuery query={graphql`
+        query{
+          component:allMarkdownRemark(
+            filter: {
+              frontmatter: {path: {regex: "/component/"}}
+            },
+            sort:{
+              fields:[frontmatter___num]
             }
-            )
+          ){
+            totalCount
+              edges{
+                node {
+                  frontmatter {
+                    title
+                    path
+                    lang
+                    num
+                  }
+                }
+            }
           }
-        </Nav>
-      </div>
-    )}
-  />
-);
+          overview:allMarkdownRemark(
+            filter: 
+              {frontmatter: {path: {regex: "/overview/"}}
+            },
+            sort:{
+              fields:[frontmatter___num]
+            }
+          ){
+            totalCount
+              edges{
+                node {
+                  frontmatter {
+                    title
+                    path
+                    lang
+                    num
+                  }
+                }
+            }
+          }
+          content:allMarkdownRemark(
+            filter: 
+            {frontmatter: {path: {regex: "/content/"}}
+            },
+            sort:{
+              fields:[frontmatter___num]
+            }
+          ) {
+            totalCount
+              edges{
+                node {
+                  frontmatter {
+                    title
+                    path
+                    lang
+                    num
+                  }
+                }
+            }
+          }
+          identity:allMarkdownRemark(
+            filter: {
+              frontmatter: {path: {regex: "/identity/"}}
+            },
+            sort:{
+              fields:[frontmatter___num]
+            }
+          ) {
+            totalCount
+              edges{
+                node {
+                  frontmatter {
+                    title
+                    path
+                    lang
+                    num
+                  }
+                }
+            }
+          }
+          data:allMarkdownRemark(
+            filter: {
+              frontmatter: {path: {regex: "/data/"}}
+            },
+            sort:{
+              fields:[frontmatter___num]
+            }
+          ){
+            totalCount
+              edges{
+                node {
+                  frontmatter {
+                    title
+                    path
+                    lang
+                    num
+                  }
+                }
+            }
+          }
+        }
+    `}
+        render={data => (
+          <div className="col-sm-auto">
+            <Nav vertical pills justify>
+              {
+                data[this.props.path.split("/")[1]].edges.map((edges) => {
+                  if ((localizer.lang === "en_CA") && (edges.node.frontmatter.lang === "en"))
+                    return (
+                      <NavItem>
+                        <Link to={edges.node.frontmatter.path} class="nav-link">
+                          {edges.node.frontmatter.title}
+                        </Link>
+                      </NavItem>
+                    );
+                  else
+                    if ((localizer.lang === "fr_CA") && (edges.node.frontmatter.lang === "fr"))
+                      return (
+                        <NavItem>
+                          <Link to={edges.node.frontmatter.path} class="nav-link">
+                            {edges.node.frontmatter.title}
+                          </Link>
+                        </NavItem>);
+                }
+                )
+              }
+            </Nav>
+          </div>
+        )}
+      />
+    )
+  }
+}
+;
 
-/* Sidenav.propTypes = {
-  data: PropTypes.string,
-};
 
-Sidenav.defaultProps = {
-  data: 'Data',
-}; */
 
 export default LocalizedComponent(Sidenav);
 
