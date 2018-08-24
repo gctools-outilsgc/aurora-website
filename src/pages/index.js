513a45be6dfa0f9034a20c1211736b399d9f49c2
Ryan Lacroix <ryan.lacroix53@gmail.com>
2018-08-24 09:51:42 -0700

    added content to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,18 @@ import {
   CardImgOverlay,
   CardBody,
   CardTitle,
-  CardSubtitle
+  CardSubtitle,
+  Jumbotron
 } from 'reactstrap';
 import Layout from '../components/layout';
+import SplashTitle from '../components/SplashTitle';
 import Img from "gatsby-image"
 import { translate } from "react-i18next";
 const IndexPage = ({ data, t, i18n }) => (
 
   <Layout>
 
-    <Card inverse className="no-container" >
+    {/*<Card inverse className="no-container" >
       <Img fluid={data.aurora.childImageSharp.fluid} />
       <CardImgOverlay className="no-borders">
         <h1 className="display-3">{t("Welcome")}</h1>
@@ -31,68 +33,102 @@ const IndexPage = ({ data, t, i18n }) => (
           <Button color="primary" onClick={() => replace('/component')}>Get started!</Button>
         </p>
       </CardImgOverlay>
-    </Card>
+</Card>*/}
+    <Jumbotron style={{backgroundImage: 'url("https://www.sciencealert.com/images/2018-02/processed/aurora_substorm_1024.jpg")', backgroundSize: 'cover', color: 'white'}}>
+        <h1 className="display-3">Aurora Design System</h1>
+        <p className="lead" style={{maxWidth: '75%', paddingBottom: '20px'}}>
+            A central design guide for the Digital Collaboration Division, the Government of Canada and our digital partners. Always free, always open and always collaborative.
+        </p>
+        <div>
+          <Button>Get started</Button>
+          <Button style={{marginLeft: '10px'}}>Documentation</Button>
+        </div>
+    </Jumbotron>
+    <Card>
+      <h2>I am a...</h2>
+      <div className="row" style={{width: '100%', justifyContent: 'space-between'}}>
+        <Card className="col text-center" style={{maxWidth: '300px'}}>
+          <Img fluid={data.eye.childImageSharp.fluid}  />
+          <CardBody>
+            <CardTitle>{t("Designer")}</CardTitle>
+            <Button outline color="primary" block>Start Sketching</Button>
+          </CardBody>
+        </Card>
+        <Card className="col text-center" style={{maxWidth: '300px'}}>
+          <Img fluid={data.gears.childImageSharp.fluid} />
+          <CardBody>
+            <CardTitle>{t("Developer")}</CardTitle>
+            <Button outline color="primary" block>Start coding</Button>
+          </CardBody>
+        </Card>
 
-    <div className="row">
-      <Card className="col text-center">
-        <Img fluid={data.eye.childImageSharp.fluid} />
-        <CardBody>
-          <CardTitle>{t("Designers")}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            One Line
-            </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card className="col text-center">
-        <Img fluid={data.gears.childImageSharp.fluid} />
-        <CardBody>
-          <CardTitle>{t("Developers")}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            One Line for testing
-            </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+        <Card className="col text-center" style={{maxWidth: '300px'}}>
+          <Img fluid={data.pen.childImageSharp.fluid} />
+          <CardBody>
+            <CardTitle>{t("Writer")}</CardTitle>
+            <Button outline color="primary" block>Start typing</Button>
+          </CardBody>
+        </Card>
 
-      <Card className="col text-center">
-        <Img fluid={data.pen.childImageSharp.fluid} />
-        <CardBody>
-          <CardTitle>{t("Writers")}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            Making this one line
-            </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-
-      <Card className="col text-center">
-        <Img fluid={data.pie.childImageSharp.fluid} />
-        <CardBody>
-          <CardTitle>{t("Data Scientists")}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            One Line
-            </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-
-      <Card className="col text-center">
-        <Img fluid={data.contributors.childImageSharp.fluid} />
-        <CardBody>
-          <CardTitle>{t("Contributors")}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>
-            One Line
-            </CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+        <Card className="col text-center" style={{maxWidth: '300px'}}>
+          <Img fluid={data.pie.childImageSharp.fluid} />
+          <CardBody>
+            <CardTitle>{t("Data Scientist")}</CardTitle>
+            <Button outline color="primary" block>Start Analyzing</Button>
+          </CardBody>
+        </Card>
     </div>
+    <h2 style={{textAlign: 'center'}}>About Aurora Design System</h2>
+    <div className="row" style={{justifyContent: 'space-between'}}>
+        <Card className="col text-left">
+          <CardBody>
+            <CardTitle>{t("Simple and flexible")}</CardTitle>
+            A leightweight system that allows individual teams to adapt branding and other design elements to suit a specific project. The system will provide guidelines for all necessary elements without being restrictive.
+          </CardBody>
+        </Card>
+        <Card className="col text-left" >
+          <CardBody>
+            <CardTitle>{t("Fun to use")}</CardTitle>
+            The system itself will have a pleasing design that is easy for developers and designers to navigate and adapt to their needs. Using the design syustem wuill make their jobs weasier rather than add an extra burden or obligations to follow.
+          </CardBody>
+        </Card>
+        <Card className="col text-left" >
+          <CardBody>
+            <CardTitle>{t("Re-usable")}</CardTitle>
+            All components and principles included in the design system are generic enough to suit a variety of needs for multiple teams. Code and design elements can easily be extracted to create new products. Communications guidelines are easy to understand and follow.
+          </CardBody>
+        </Card>
+    </div>
+    <div className="row" style={{justifyContent: 'space-between'}}>
+        <Card className="col text-left">
+          <CardBody>
+            <CardTitle>{t("Diverse")}</CardTitle>
+            The design system will cover a variety of needs and types of products. The system will also cover necessary government obligations such as official languages and accessibility.
+          </CardBody>
+        </Card>
+        <Card className="col text-left" >
+          <CardBody>
+            <CardTitle>{t("Technology agnostic")}</CardTitle>
+            The design system is applicable regardless of which technology framework the team decides to use for their project.
+          </CardBody>
+        </Card>
+        <Card className="col text-left" >
+          <CardBody>
+            <CardTitle>{t("Open")}</CardTitle>
+            The design system will be open and accessible to anyone who wishes to use it. All code is open source, and other guidelines and elements are free to copy.
+          </CardBody>
+        </Card>
+    </div>
+    </Card>
+    <Jumbotron className="text-right" style={{backgroundImage: 'url("https://www.sciencealert.com/images/2018-02/processed/aurora_substorm_1024.jpg")', backgroundSize: 'cover', color: 'white', textAlign: 'right'}}>
+    <p className="lead" style={{display: 'inline-block', maxWidth: '50%', paddingBottom: '10px', textAlign: 'left'}}>
+      Don’t fall into one of the roles above? The Aurora Design System can still improve your work! We are always looking for contributors to make the Aurora Design System better for content creators.
+    </p>
+    <p>
+      <Button>Learn more</Button>
+    </p>
+
+    </Jumbotron>
 
   </Layout>
 
