65815e4f321f73801d7bdbfda4ce043ba0fb4925
Addyvan <avandenhoeve14@ubishops.ca>
2018-08-24 14:58:27 -0400

    Cleaned up the LanguageSwitcher code
    Made it a toggle instead and shortened/simplified the code since it was confusing me.

--- src/components/languageSwitch.js
+++ src/components/languageSwitch.js
@@ -5,51 +5,33 @@ import { translate } from "react-i18next";
 class LanguageSwitcher extends Component {
   constructor(props) {
     super(props);
-    const { i18n } = this.props;
-    this.state = { language: i18n.language };
+    this.state = { isEnglish: (props.i18n.language === "en") ? true : false };
 
     this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ language: nextProps.i18n.language });
-  }
-
   handleChangeLanguage() {
-    const { i18n } = this.props;
-    if (this.state.language == "en") {
-      i18n.changeLanguage("fr");
-    } else {
-      i18n.changeLanguage("en");
-    }
-  }
-
-  renderLanguageChoice() {
-
-    if (this.state.language == "en")
-      var lang = "FR";
-    else 
-      var lang = "EN";
-
-    return (
-      <Button
-        className="languageSwitcherButton"
-        onClick={this.handleChangeLanguage}
-        size="lg"
-        style={ {"font-weight": "550"} }
-      >
-      {lang}
-      </Button>
-    );
+    this.props.i18n.changeLanguage(this.state.isEnglish ? "fr" : "en");
+    this.setState(prevState => ({
+      isEnglish: !prevState.isEnglish
+    }));
   }
 
   render() {
     return (
       <div className="LanguageSwitcher">
-        {this.renderLanguageChoice(this.state.language)}
+        <Button
+          className="languageSwitcherButton"
+          onClick={this.handleChangeLanguage}
+          size="lg"
+          style={ {"font-weight": "550"} }
+        >
+        {this.state.isEnglish ? 'FR' : 'EN'}
+        </Button>
       </div>
     );
   }
+
 }
 
 export default translate("LanguageSwitcher")(LanguageSwitcher);
