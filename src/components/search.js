6c3d0a782144d2e6a7ee60bcbb4e5c8b61132e85
Addison van den Hoeven <AVANDENHOEVE14@ubishops.ca>
2018-09-17 12:39:36 -0400

    Reprogrammed search to work with IE

--- src/components/search.js
+++ src/components/search.js
@@ -8,16 +8,18 @@ import {
     ListGroupItem,
     Label
 } from 'reactstrap';
+import {indexEN, indexFR, searchValues} from './SearchIndex';
 
 // Search component
 export default class Search extends Component {
     constructor(props) {
         super(props)
         this.toggle = this.toggle.bind(this);
+        this.getSearchResults = this.getSearchResults.bind(this);
         this.state = {
-            query: ``,
-            results: [],
-            dropdownOpen: false,
+          query: ``,
+          results: [],
+          dropdownOpen: false,
         }
     }
 
@@ -28,33 +30,34 @@ export default class Search extends Component {
     }
 
     getSearchResults(query) {
-        if (!query || !window.__LUNR__) return [];
-        this.setState({
-            dropdownOpen: true,
-        })
-        const lunrIndex = window.__LUNR__[this.props.lng];
-        /* const results = lunrIndex.index.query(function () { // you can  customize your search , see https://lunrjs.com/guides/searching.html
-
-            // look for an exact match and apply a large positive boost
-            this.term(query);
-
-            // look for terms that match the beginning of this queryTerm and apply a medium boost
-            this.term(query + "*");
-
-            // look for terms that match with an edit distance of 2 and apply a small boost
-            this.term(query + "~2");
-        }); */
-        const results = lunrIndex.index.search(`${query}^100 ${query}*^10 ${query}~2`);
-        return results.map(({ ref }) => lunrIndex.store[ref]);
+      if (query.length > 0) {
+        if (this.props.lng == "en") {
+          return indexEN.search(`${query}^100 ${query}*^10 ${query}~2`);
+        } else {
+          return indexFR.search(`${query}^100 ${query}*^10 ${query}~2`);
+        }
+      } else {
+        return null;
+      }
+      
     }
 
     search = event => {
         const query = event.target.value;
         const results = this.getSearchResults(query);
-        this.setState({
+        
+        if (results) {
+          this.setState({
             results: results.slice(0, 5),
             query,
-        })
+          })
+        } else {
+          this.setState({
+            results: [],
+            query,
+          })
+        }
+        
     }
 
     render() {
@@ -76,14 +79,17 @@ export default class Search extends Component {
               {(this.state.results.length !== 0) ?
 
                 this.state.results.map(page =>
-                    <ListGroupItem tag={Link} to={page.path}>
-                        {page.title}
+                    <ListGroupItem tag={Link} to={page.ref}>
+                        {searchValues[page.ref]}
                     </ListGroupItem>
                 )
                 :
+                (this.state.query.length > 0) ?
                 <ListGroupItem toggle={"false"}>
                     No results were found.
                 </ListGroupItem>
+                :
+                null
               }
             </DropdownMenu>
           </Dropdown>
